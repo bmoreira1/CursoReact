4b7d5a5017c41dfafabc4f2c1cd7774350365c7b
bruno moreira <bbmoreira.195@gmail.com>
2022-02-21 23:11:45 -0300

    correção de ortografia no código

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 x = 0 
 
 while (x<10) {
-    document.write ("</br> Ovalor de x: " + x);
+    document.write ("</br> O valor de x: " + x);
 
     x++
 }
@@ -16,3 +16,5 @@ for(a = 0; a <10; a++){
 
     document.write("</br> O valor de A: " + a);
 }
+
+document.write("</br></br></br> o A esta valendo: "+ a +"</br></br>");
