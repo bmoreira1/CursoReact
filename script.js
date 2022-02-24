8bb097368a3b090dc02774e07362fb621e5823ab
bruno moreira <bbmoreira.195@gmail.com>
2022-02-23 21:07:25 -0300

    aprendizado com temporização e consoleLog

--- script.js
+++ script.js
@@ -1,33 +1,12 @@
-//Switch 
-document.write("</br>Escolha o seu pedido: </br></br>");
-document.write("</br>1 = Cachorro Quente </br></br>");
-document.write("</br>2 = Refrigerante </br></br>");
-document.write("</br>3 = Hamburguer</br></br>");
-document.write("</br>4 = Suco de Limão</br></br>");
-
-function pedir(){
-
-    x = prompt("o que deseja pedir? ");
-
-    switch(x){
-
-        case '1':
-            alert("Seu pedido é Cachorro Quente! ");
-            brake;
-            
-        case '2':
-            alert ("Seu pedido é Refrigerante! ");
-            brake;
-        case '3':
-            alert ("Seu pedido é Hamburguer! ");
-            brake;
-
-        case '4':
-            alert ("Seu pedido é Suco de Limão! ");
-            brake;
-        default:
-            alert("Ops. Não temos essa opção!");
-            break;
-
-    }
+function acao(){
+    document.write("executando...</br>");
 }
+
+//executa de tempo em tempo!
+//setInterval(acao, 1000);
+
+//executa após o tempo estipulado
+//setTimeout(acao, 3000);
+
+
+var timer = setInterval(acao, 1000)
