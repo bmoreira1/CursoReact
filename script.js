19a247a0148acdb3197b1a4ee619fd6501fd33b3
bruno moreira <bbmoreira.195@gmail.com>
2022-03-09 11:17:55 -0300

    modulo Spread Operator / Rest Operator

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
-function acao(){
-    document.write("executando...</br>");
+
+/*
+function cadastroPessoa(info){ -->
+    let novosDados = {
+        ...info,
+        cargo: 'programador',
+        status: 1,
+        codigo:202102191051
+    }
+
+    return novosDados;
+
+}
+console.log(cadastroPessoa({nome: 'Bruno', sobrenome: 'Barbosa', anoInicio: 2022}));*/
+
+
+function cadastrar(usuarios, ...novosUsuarios){
+
+    let totalusuarios = [
+        ...usuarios,
+        ...novosUsuarios
+    ];
+    return console.log(totalusuarios);
+
 }
 
-//executa de tempo em tempo!
-//setInterval(acao, 1000);
+let usuarios = ["Bruno", "Estefany"];
 
-//executa após o tempo estipulado
-//setTimeout(acao, 3000);
-
-
-var timer = setInterval(acao, 1000)
+let novosUsuarios = cadastrar(usuarios, "Jessica", "Bia");
